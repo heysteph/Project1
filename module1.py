387c7431d3e1eda00a6fe6ee0afe8130ee9a4760
heysteph <stephanietanasia@hotmail.com>
2022-10-04 22:13:19 -0400

    4oct

--- module1.py
+++ module1.py
@@ -1,4 +1,6 @@
 from math import sqrt
+import module1 as mp
+
 def dist(a,b):
     dist = sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
     return dist
@@ -13,18 +15,17 @@ def mergexy(trial):
         trial["Player"+str(x)] = tuple(zip(*trial[['Player'+str(x)+'_x','Player'+str(x)+'_y']].values.T))
     return trial
 
-def counter(trial):
-    count=[]
-    home = list(range(1,15))
-    away = list(range(15,27))
-    for i in range(0,963):
-        s = trial["startpasses"].iloc[i]
-        d = []
-    if s < 15:
-        for j in away:
-            d.append(mp.dist(trial["Player"+str(s)].iloc[i], trial["Player"+str(j)].iloc[i] ))
-    else:
-        for j in home:
-            d.append(mp.dist(trial["Player"+str(s)].iloc[i], trial["Player"+str(j)].iloc[i] ))
-    count.append(sum(k < 10 for k in d))
-    return count
+def types(passes):
+    classes=[]
+    for value in passes:
+        if value == "REG":
+            classes.append(1)
+        elif value =="HEAD":
+            classes.append(2)
+        elif value =="CROSS":
+            classes.append(3)
+        elif value =="DEEP BALL":
+            classes.append(4)
+        else:
+            classes.append(5)
+    return classes
